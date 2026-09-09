6be788b407c7ba48fad74ceabaf87821647f1131
NiHoffmann <nils.18.09.97@gmail.com>
2026-09-09 17:42:36 +0200

    moved robot with control stript

---
+++ playground/connection_to_real_robot_working_kind_of_its_alive/env.bat
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# ===== User configuration =====
+HUMBLE_PATH="/opt/ros/humble"
+ROS2_PATH="$HOME/ros2_ws"
+
+# Allow use to override linuxbrew/other pythons shadowing system python (breaks rclpy for me)
+# uncomment if not needed.
+export PATH="/usr/bin:$PATH"
+
+# ===== Environment setup =====
+source "$HUMBLE_PATH/setup.bash"
+source "$ROS2_PATH/install/setup.bash"
+
+# Add src to PYTHONPATH so python can find the modules
+ENV_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export PYTHONPATH="$PYTHONPATH:$ENV_DIR/src"
+
+export FASTRTPS_DEFAULT_PROFILES_FILE=~/ros2_network_config.xml
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
